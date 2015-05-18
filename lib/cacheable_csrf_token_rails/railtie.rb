2d30fa7c9e7ce480b24836c5b2116f5091e91bf3
Toby Marsden <toby@toby.org.uk>
2015-05-18 22:21:33 +0200

    Disable middleware for debugging

--- lib/cacheable_csrf_token_rails/railtie.rb
+++ lib/cacheable_csrf_token_rails/railtie.rb
@@ -2,7 +2,7 @@
 module CacheableCsrfTokenRails
   class Railtie < Rails::Railtie
     initializer "cacheable_csrf_token_rails.use_rack_middleware" do |app|
-      app.config.middleware.insert_after "Rack::ETag", "::CacheableCsrfTokenRails::Middleware"
+      #app.config.middleware.insert_after "Rack::ETag", "::CacheableCsrfTokenRails::Middleware"
     end
   end
 end
